803d3c6ccefb6401b7a5a160d575ab5e45467356
Benjamin Devillers <benjamin.devillers@supelec.fr>
2019-02-16 13:07:42 +0100

    update readme and add qpsolvers as default solver for svm

--- README.md
+++ README.md
@@ -19,13 +19,14 @@ Simple onehot où une lettre représenté un vecteur de dim 4 onehot.
 Pas d'args.
 
 #### spectrum
-Spectre de la sequence, inspiré de slide 55 de [http://members.cbio.mines-paristech.fr/~jvert/talks/060727mlss/mlss.pdf](http://members.cbio.mines-paristech.fr/~jvert/talks/060727mlss/mlss.pdf)
+Spectre de la sequence, inspiré de slide 55 de [http://members.cbio.mines-paristech.fr/~jvert/talks/060727mlss/mlss.pdf](http://members.cbio.mines-paristech.fr/~jvert/talks/060727mlss/mlss.pdf).
+
 Args :
 - length : taille de la fenêtre glissante
 
 
 ### classifiers
-Config des classfieurs (à ajouter au fur et à mesure)
+Config des classfieurs (à ajouter au fur et à mesure).
 Parmi :
 
 #### svm
--- classifiers/svm.py
+++ classifiers/svm.py
@@ -1,4 +1,5 @@
 import numpy as np
+from qpsolvers import solve_qp
 from classifiers import Classifier
 
 
@@ -8,8 +9,17 @@ class SVMClassifier(Classifier):
         self.lbd = lbd
 
     def fit(self):
-        def hinge(x):
-            return np.max(1 - x, 0)
-
-        # TODO: subgradient descent
+        """
+        Solve $\min_x (1/2) x^T K x - yx$
+        subject to
+        $Gx \le h$
+        """
+        n = self.training_data.shape[0]
+        K = self.train_K
+        K = K + np.eye(K.shape[0]) * 1e-5  # Otherwise not positive definite
+        y = self.training_data['Bound'].values.astype(float)
+        G = np.diag(y)
+        G = np.concatenate([G, -G], axis=0)
+        h = np.concatenate([1 / (2 * self.lbd * n) * np.ones(n), np.zeros(n)]).reshape((2 * n,))
+        self.alpha = solve_qp(K, -y, G, h)
         return self.alpha
