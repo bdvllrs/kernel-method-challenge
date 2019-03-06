Fix local-alignment issue. Less computation for gram matrix. Add tqdm for the gram matrix computation.

kernels/default.py
@@ -1,6 +1,7 @@
 import numpy as np
 import hashlib
 from utils.memoizer import Memoizer
+from tqdm import tqdm
 
 __all__ = ['Kernel', 'OneHotKernel']
 
@@ -33,6 +34,7 @@ class Kernel:
             data_1: array of embeddings
             data_2: (optional) array of embeddings 2. Default: data_2 = data_1.
         """
+        is_same_data = data_2 is None
         if data_2 is None:
             data_2 = data_1
         # Get hashes for memoization
@@ -46,7 +48,17 @@ class Kernel:
             print('Using memoized data.')
             path = "gram.{}.{}".format(hash_2, hash_1)
             return self.memoizer[path]
-        gram = np.array([[self.apply(x, y, i, j) for i, x in enumerate(data_1)] for j, y in enumerate(data_2)])
+        gram = np.ones((len(data_1), len(data_2))) * -1
+        print("Computing gram...")
+        with tqdm(total=len(data_1) * len(data_2)) as progress_bar:
+            for j, y in enumerate(data_2):
+                for i, x in enumerate(data_1):
+                    if not is_same_data or j <= i:
+                        gram[j, i] = self.apply(x, y, i, j)
+                        progress_bar.update(1)
+                    if is_same_data and j <= i:
+                        gram[i, j] = gram[j, i]
+                        progress_bar.update(1)
         self.memoizer["gram.{}.{}".format(hash_1, hash_2)] = gram
 
         return gram

kernels/local_alignment.py
@@ -32,97 +32,66 @@ class LocalAlignmentKernel(Kernel):
         self.X2 = None
         self.Y2 = None
 
-        self.frequencies = None
-        self.couple_frequencies = None
+        self.S = None
         self.gap = lambda n: 0 if n == 0 else d + e * (n - 1)
 
     def score(self, x: str, y: str):
-        couple_freq = 0
-        if x + y in self.couple_frequencies.keys():
-            couple_freq = self.couple_frequencies[x + y]
-        return np.log(couple_freq / (self.frequencies[x] * self.frequencies[y]))
+        if not x + y in self.S.keys():
+            return 0
+        return self.S[x + y]
 
     def compute_frequencies(self, sequences):
         """
         Inspired from https://www.cs.cmu.edu/~02710/Lectures/ScoringMatrices2015.pdf
         and https://en.wikipedia.org/wiki/BLOSUM
         """
-        self.frequencies = {"A": 0, "C": 0, "G": 0, "T": 0}
-        self.couple_frequencies = {}
+        frequencies = {"A": 0, "C": 0, "G": 0, "T": 0}
+        couple_frequencies = {}
+        self.S = {}
         total = 0
         total_couple = 0
         for sequence in sequences:
             for letter in sequence:
-                self.frequencies[letter] += 1
+                frequencies[letter] += 1
                 total += 1
             for k in range(0, len(sequence) - 1, 2):
                 couple = sequence[k:k + 2]
-                if couple not in self.couple_frequencies.keys():
-                    self.couple_frequencies[couple] = 1
+                if couple not in couple_frequencies.keys():
+                    couple_frequencies[couple] = 1
                 else:
-                    self.couple_frequencies[couple] += 1
+                    couple_frequencies[couple] += 1
                 total_couple += 1
-        for key in self.frequencies.keys():
-            self.frequencies[key] /= total
-        for key in self.couple_frequencies.keys():
-            self.couple_frequencies[key] /= total_couple
+        for key in frequencies.keys():
+            frequencies[key] /= total
+        for key in couple_frequencies.keys():
+            couple_frequencies[key] /= total_couple
+            self.S[key] = np.log(couple_frequencies[key] / (frequencies[key[0]] * frequencies[key[1]]))
 
-    def get_M(self, i, j, x1, x2):
-        if i == 0 or j == 0:
-            return 0
-        elif self.M[i][j] is None:
-            score = np.exp(self.beta * self.score(x1[i - 1], x2[j - 1]))
-            self.M[i][j] = (score * (1 + self.get_X(i - 1, j - 1, x1, x2) +
-                                     self.get_Y(i - 1, j - 1, x1, x2) + self.get_M(i - 1, j - 1, x1, x2)))
-        return self.M[i][j]
+    def apply(self, x1, x2, _, __):
+        size = len(x1) + 1
+        M = np.zeros((size, size))
+        X = np.zeros((size, size))
+        Y = np.zeros((size, size))
+        X2 = np.zeros((size, size))
+        Y2 = np.zeros((size, size))
 
-    def get_X(self, i, j, x1, x2):
-        if i == 0 or j == 0:
-            return 0
-        elif self.X[i][j] is None:
-            self.X[i][j] = (np.exp(self.beta * self.d) * self.get_M(i - 1, j, x1, x2)
-                            + np.exp(self.beta * self.e) * self.get_X(i - 1, j, x1, x2))
-        return self.X[i][j]
+        exp_beta_d = np.exp(self.beta * self.d)
+        exp_beta_e = np.exp(self.beta * self.e)
 
-    def get_Y(self, i, j, x1, x2):
-        if i == 0 or j == 0:
-            return 0
-        elif self.Y[i][j] is None:
-            self.Y[i][j] = (np.exp(self.beta * self.d) * (self.get_M(i, j - 1, x1, x2) + self.get_X(i, j - 1, x1, x2))
-                            + np.exp(self.beta * self.e) * self.get_Y(i, j - 1, x1, x2))
-        return self.Y[i][j]
+        for i in range(1, size):
+            for j in range(1, size):
+                score = np.exp(self.beta * self.score(x1[i - 1], x2[j - 1]))
+                M[i, j] = score * (1 + X[i - 1, j - 1] + Y[i - 1, j - 1] + M[i - 1, j - 1])
+                X[i, j] = exp_beta_d * M[i - 1, j] + exp_beta_e * X[i - 1, j]
+                Y[i, j] = exp_beta_d * (M[i, j - 1] + X[i, j - 1]) + exp_beta_e * Y[i, j - 1]
+                X2[i, j] = M[i - 1, j] + X2[i - 1, j]
+                Y2[i, j] = M[i, j - 1] + X2[i, j - 1] + Y2[i, j - 1]
 
-    def get_X2(self, i, j, x1, x2):
-        if i == 0 or j == 0:
-            return 0
-        elif self.X2[i][j] is None:
-            self.X2[i][j] = self.get_M(i - 1, j, x1, x2) + self.get_X2(i - 1, j, x1, x2)
-        return self.X2[i][j]
-
-    def get_Y2(self, i, j, x1, x2):
-        if i == 0 or j == 0:
-            return 0
-        elif self.Y2[i][j] is None:
-            self.Y2[i][j] = self.get_M(i, j - 1, x1, x2) + self.get_X2(i, j - 1, x1, x2) + self.get_Y2(i, j - 1, x1, x2)
-        return self.Y2[i][j]
-
-    def apply(self, embed1, embed2, _, __):
-        assert self.frequencies is not None, "No sequence has been embedded before applying the kernel."
-        size = len(embed1)
-        self.M = [[None] * (size + 1)] * (size + 1)
-        self.X = [[None] * (size + 1)] * (size + 1)
-        self.Y = [[None] * (size + 1)] * (size + 1)
-        self.X2 = [[None] * (size + 1)] * (size + 1)
-        self.Y2 = [[None] * (size + 1)] * (size + 1)
-
-        val = (1 + self.get_X2(size, size, embed1, embed2)
-               + self.get_Y2(size, size, embed1, embed2) + self.get_M(size, size, embed1, embed2))
-        val = 1 / self.beta * np.log(val)  # resole diagonal dominance issue
-        print(val)
-        return val
+        val = 1 + X2[size - 1, size - 1] + Y2[size - 1, size - 1] + M[size - 1, size - 1]
+        return 1 / self.beta * np.log(val)  # resole diagonal dominance issue
 
     def embed(self, sequences):
-        if self.frequencies is None:
+        if self.S is None:
             self.compute_frequencies(sequences)
         return sequences
 